b342bd8746e4066d858372936b94f6f1d298b33d
pureevil666 <86472191+pureevil666@users.noreply.github.com>
2022-10-20 14:04:14 +0300

    добавление массива со значениями в словарь категорий

--- calculation.py
+++ calculation.py
@@ -15,17 +15,34 @@ def convert_data_to_list(data):
 
 def create_groups(input_list):
     global group_dict
+    group_list = []
     for line in input_list:
         if len(line.strip(LETTERS)) == 0:
             group_dict[line] = 0
-    save_indexes(group_dict, input_list)
+    for name in group_dict:
+        group_list.append(name)
+    save_indexes(group_dict, input_list, group_list)
 
 
-def save_indexes(group_dict, input_list):
-    print(input_list)
-    print(group_dict)
+def save_indexes(group_dict, input_list, group_list):
     index_list = []
     for group in group_dict:
         index = input_list.index(group)
         index_list.append(index)
+    split_list(index_list, group_dict, input_list, group_list)
+
+
+def split_list(index_list, group_dict, input_list, group_list):
+    for i in range(0, len(index_list)):
+        start = index_list[i]
+        if i < len(index_list) - 1:
+            stop = index_list[i + 1]
+            saved_list = input_list[start + 1:stop]
+        else:
+            saved_list = input_list[start + 1:]
+        group_dict[group_list[i]] = saved_list
+    print(input_list)
+    print(group_dict)
     print(index_list)
+    print(group_list)
+    print(group_dict)
