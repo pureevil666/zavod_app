2352dd3f0a2ab9014f71269bfc7946f64e1afc76
pureevil666 <86472191+pureevil666@users.noreply.github.com>
2022-10-20 13:29:28 +0300

    сохранение индексов категорий

--- calculation.py
+++ calculation.py
@@ -2,6 +2,7 @@ LETTERS = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя-'
 LETTERS = LETTERS + LETTERS.upper()
 group_dict = {}
 
+
 def give_data(data):
     input_text = data
     convert_data_to_list(input_text)
@@ -9,12 +10,22 @@ def give_data(data):
 
 def convert_data_to_list(data):
     data_list = data.splitlines()
-    print(data_list)
     create_groups(data_list)
+
 
 def create_groups(input_list):
     global group_dict
     for line in input_list:
         if len(line.strip(LETTERS)) == 0:
             group_dict[line] = 0
+    save_indexes(group_dict, input_list)
+
+
+def save_indexes(group_dict, input_list):
+    print(input_list)
     print(group_dict)
+    index_list = []
+    for group in group_dict:
+        index = input_list.index(group)
+        index_list.append(index)
+    print(index_list)
